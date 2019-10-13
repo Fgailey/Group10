66b961246771af2b3907f7eadafeca067b816c35
Scott Ladd <scottjr101@gmail.com>
2019-10-13 15:44:32 -0400

    huge waste of time, modals are dumb

--- assets/js/application.js
+++ assets/js/application.js
@@ -7,11 +7,9 @@
 //     }); 
 // end of document ready
 //Materialize Modals
-$(document).on('DOMContentLoaded', () => {
-
-  let modals = $('.modal');
+$(document).ready(function(){
+  $('.modal').modal();
   M.Modal.init(modals);
-
 });
 $(document).ready(() => {
 
@@ -22,7 +20,7 @@ $(document).ready(() => {
     var startDate = $("#start-date-input").val().trim();
     var zipCode = $("#zip-code-input").val().trim();
     var radius = $("#radius-input").val().trim();
-    var queryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" + startDate + "&zip=" + zipCode + "&radius=" + radius + "&api_key=h22mr6gbzmesjmx4jb5qt67b"
+    var queryURL = "https://data.tmsapi.com/v1.1/movies/showings?startDate=" + startDate + "&zip=" + zipCode + "&radius=" + radius + "&api_key=h22mr6gbzmesjmx4jb5qt67b"
 
     $.ajax({
       url: queryURL,
@@ -30,7 +28,7 @@ $(document).ready(() => {
     }).then(function (response) {
       console.log(response);
       $("#movies-view").empty();
-      for (var i = 0; i < 100; i++) {
+      for (var i = 0; i < 10; i++) {
         var div = $("<div class='movie_view'>");
         var title = response[i].title;
         var p = $("<p>").text(title);
@@ -40,18 +38,44 @@ $(document).ready(() => {
         image.attr('src', poster);
         div.append(image);
         div.append('<br>');
-        var a = $("<a href='../../showtimes.html'>");
+        var a = $("<a href='#' class='black-text modal-trigger'>");
+        a.attr('data-type', 'modal-' + [i]);
         a.text("Click here for showtimes");
         div.append(a);
+        var divModal = $("<div class='modal'>");
+        divModal.attr('id', 'modal-' + [i]);
+        divModal.text('Showtimes');
+        div.append(divModal);
         $("#movies-view").append(div);
-
       };
 
+      var modal = function(){
+        $('.modal').modal();
+        M.Modal.init(modals);
+      };
 
+      $(document).on("click", ".black-text modal-trigger", modal);
 
     });
 
   });
+
+  // $(".track").click(function () {
+  //   console.log(response);
+  //   for (var j = 0; j < 100; j++) {
+  //     var divShowview = $("<div class='showArea'>");
+  //     divShowview.text(response[i].showtimes[j].dateTime);
+  //     $("#showtimes-view").append(divShowview);
+  //   };
+  // });
+
+  // var showtimes = function(){
+  //   for (var j = 0; j < 100; j++) {
+  //     var divShowview = $("<div class='showArea'>");
+  //     divShowview.text(response[i].showtimes[j].dateTime);
+  //     $("#showtimes-view").append(divShowview);
+  //   };
+  // };
 
   $("#add-event-btn").click((ev) => {
 
